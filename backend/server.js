34fb2cf8c7693cbd9cdd87a85fb2976ae4da3ae3
Junaid SIkander Hakro <Junaid_sikander@live.com>
2020-12-28 18:07:16 +0500

    [SETUP] backend

---
+++ backend/server.js
@@ -0,0 +1,13 @@
+import express from 'express'
+import dotenv from 'dotenv'
+
+
+dotenv.config();
+const app = express();
+const PORT = process.env.PORT || 5000;
+
+app.get('/', (req, res) => {
+    res.send("Checking Api....")
+});
+
+app.listen(PORT, console.log(`Server running in ${process.env.NODE_ENV} mode on port ${PORT} `))
