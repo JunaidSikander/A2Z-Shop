7377fe6e6729f9ca749d510ede13d57e779daaf2
Junaid SIkander Hakro <Junaid_sikander@live.com>
2020-12-28 20:42:13 +0500

    [ADD] data seeder script for sample data

---
+++ backend/data/users.js
@@ -0,0 +1,26 @@
+import bcrypt from 'bcryptjs'
+
+const users = [
+    {
+        name: "Admin User",
+        email: 'admin@example1.com',
+        password: bcrypt.hashSync('12345', 10),
+    },
+    {
+        name: "Junaid Ali",
+        email: 'admin@example2.com',
+        password: bcrypt.hashSync('12345', 10),
+    },
+    {
+        name: "Moin uddin",
+        email: 'admin@example3.com',
+        password: bcrypt.hashSync('12345', 10),
+    },
+    {
+        name: "Aniss Rehman",
+        email: 'admin@example4.com',
+        password: bcrypt.hashSync('12345', 10),
+    },
+];
+
+export default users;
