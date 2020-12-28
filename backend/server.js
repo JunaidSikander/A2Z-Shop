5d7bbe144cd18837904b5f71a82cf17460184031
Junaid SIkander Hakro <Junaid_sikander@live.com>
2020-12-28 22:06:07 +0500

    [ADD] routes to fetch products from database

--- backend/server.js
+++ backend/server.js
@@ -1,6 +1,7 @@
 import express from 'express'
 import dotenv from 'dotenv'
 import connectDB from "./config/db.js";
+import productRouter from "./routes/productRoutes.js";
 
 
 dotenv.config();
@@ -9,8 +10,6 @@ connectDB();
 const app = express();
 const PORT = process.env.PORT || 5000;
 
-app.get('/', (req, res) => {
-    res.send("Checking Api....")
-});
+app.use('/api/product',productRouter);
 
-app.listen(PORT, console.log(`Server running in ${process.env.NODE_ENV} mode on port ${PORT} `))
+app.listen(PORT, console.log(`Server running in ${process.env.NODE_ENV} mode on port ${PORT} `));
