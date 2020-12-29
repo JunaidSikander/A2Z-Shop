6213acad1e33b4c52bb50daf4c894509927e9900
Junaid SIkander Hakro <Junaid_sikander@live.com>
2020-12-29 10:20:27 +0500

    [FETCH] data from database with axios

--- frontend/src/screens/ProductScreen.js
+++ frontend/src/screens/ProductScreen.js
@@ -1,11 +1,20 @@
-import React from 'react'
+import React, {useEffect, useState} from 'react'
 import {Link} from 'react-router-dom'
 import {Button, Col, Image, ListGroup, Row} from 'react-bootstrap'
 import Rating from "../components/Rating";
-import products from "../products";
+import axios from "axios";
 
 const ProductScreen = ({match}) => {
-    const product = products.find(p => p._id === match.params.id);
+    const [product, setProduct] = useState([]);
+
+    useEffect(() => {
+        const fetchProduct = async () => {
+            const {data} = await axios.get(`/api/products/${match.params.id}`);
+            setProduct(data);
+        };
+
+        fetchProduct();
+    }, [match]);
     return (
         <>
             <Link className='btn btn-light my-3' to='/'>GO Bach</Link>
