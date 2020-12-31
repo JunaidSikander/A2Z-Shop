636414b3f7a7e2c0288f5830d74cc44bdf34435b
Junaid Sikander <Junaid_sikander@live.com>
2020-12-31 23:53:14 +0500

    [FIX] user router path

--- backend/server.js
+++ backend/server.js
@@ -2,9 +2,9 @@ import express from 'express'
 import dotenv from 'dotenv'
 import connectDB from "./config/db.js";
 import productRouter from "./routes/productRoutes.js";
+import userRouter from "./routes/userRoutes.js";
 
 import {errorHandler, notFound} from "./middlewares/errorMiddleware.js";
-import {authUser} from "./controllers/userController.js";
 
 
 dotenv.config();
@@ -15,7 +15,7 @@ const PORT = process.env.PORT || 5000;
 
 app.use(express.json());
 app.use('/api/products', productRouter);
-app.use('/api/users', authUser);
+app.use('/api/users', userRouter);
 app.use(notFound);
 app.use(errorHandler);
 
