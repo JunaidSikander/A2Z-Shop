b13208fcf122334a055fa577043dce4f28282a9f
Junaid Sikander <Junaid_sikander@live.com>
2021-01-01 11:48:12 +0500

    [ADD] resgister controller and route

--- backend/controllers/userController.js
+++ backend/controllers/userController.js
@@ -2,6 +2,40 @@ import asyncHandler from 'express-async-handler'
 import generateToken from "../utils/generateToken.js";
 import User from "../models/userModel.js";
 
+//@desc     Register new User
+//@route    POST /api/users
+//@access   Pubic
+const registerUser = asyncHandler(async (req, res) => {
+    const {_id ,name,email,password, isAdmin} = req.body;
+
+    const userExists = await  User.findOne({email});
+
+    if(userExists){
+        res.status(400);
+        throw new Error('User already Exists');
+    }
+
+    const user = await User.create({
+       name,
+       email,
+       password
+    });
+
+    if(user){
+        res.status(200).json({
+            _id: user._id,
+            name: user.name,
+            email: user.email,
+            isAdmin: user.isAdmin,
+            token: generateToken(user._id)
+        })
+    }else {
+        res.status(400);
+        throw new Error('Invalid user data');
+    }
+
+});
+
 //@desc     Auth user & get token
 //@route    POST /api/users/login
 //@access   Pubic
@@ -43,4 +77,4 @@ const getUserProfile = asyncHandler(async (req, res) => {
 
 });
 
-export {authUser, getUserProfile}
+export {authUser, getUserProfile, registerUser};
--- backend/routes/userRoutes.js
+++ backend/routes/userRoutes.js
@@ -1,11 +1,12 @@
 import express from 'express'
-import {authUser, getUserProfile} from "../controllers/userController.js";
+import {authUser, getUserProfile, registerUser} from "../controllers/userController.js";
 import {protect} from "../middlewares/authMiddleware.js";
 
 
 const userRouter = express.Router();
 
-userRouter.post('/login',authUser);
+userRouter.post('/login', authUser);
+userRouter.post('/', registerUser);
 userRouter.route('/profile').get(protect, getUserProfile);
 
 export default userRouter;
