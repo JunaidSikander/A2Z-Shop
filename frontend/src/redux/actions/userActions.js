481e9f93673eebeb7b05dde5599f4b294e3131de
Junaid Sikander <Junaid_sikander@live.com>
2021-01-01 17:50:50 +0500

    [ADD] register actions, constants and reducers

--- frontend/src/redux/actions/userActions.js
+++ frontend/src/redux/actions/userActions.js
@@ -1,5 +1,13 @@
 import axios from "axios";
-import {USER_LOGIN_FAIL, USER_LOGIN_REQUEST, USER_LOGIN_SUCESS, USER_LOGOUT} from "../constants/userConstants";
+import {
+    USER_LOGIN_FAIL,
+    USER_LOGIN_REQUEST,
+    USER_LOGIN_SUCESS,
+    USER_LOGOUT,
+    USER_REGISTER_FAIL,
+    USER_REGISTER_REQUEST,
+    USER_REGISTER_SUCCESS
+} from "../constants/userConstants";
 
 export const login = (email, password) => async (dispatch) => {
     try {
@@ -32,4 +40,37 @@ export const login = (email, password) => async (dispatch) => {
 export const logout = () => dispatch => {
     localStorage.removeItem('userInfo');
     dispatch({type: USER_LOGOUT})
+};
+
+export const register = (name, email, password) => async (dispatch) => {
+    try {
+        dispatch({type: USER_REGISTER_REQUEST});
+
+        const config = {
+            headers: {
+                'Content-Type': 'application/json'
+            }
+        };
+
+        const {data} = await axios.post('/api/users', {name, email, password}, config);
+
+        dispatch({
+            type: USER_REGISTER_SUCCESS,
+            payload: data,
+        });
+
+        dispatch({
+            type: USER_LOGIN_SUCESS,
+            payload: data
+        });
+
+        localStorage.setItem('userInfo', JSON.stringify(data));
+    } catch (e) {
+        dispatch({
+            type: USER_REGISTER_FAIL,
+            payload: e.response && e.response.data.message ?
+                e.response.data.message : e.message
+        })
+    }
+
 };
