5e6e615d7c3fb9733f2735a1d67e71dd6f4cb740
Junaid Sikander <Junaid_sikander@live.com>
2021-01-01 16:45:26 +0500

    [ADD] form container for login screen

---
+++ frontend/src/components/FormContainer.js
@@ -0,0 +1,14 @@
+import React from 'react'
+import {Col, Container, Row} from 'react-bootstrap'
+
+const FormContainer = ({children}) => (
+    <Container>
+        <Row className='justify-content-md-center'>
+            <Col xs={12} md={6}>
+                {children}
+            </Col>
+        </Row>
+    </Container>
+);
+
+export default FormContainer;
