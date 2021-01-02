5f7ef87b60f1bbfa58985ab28c9390bc7fa1a209
Junaid Sikander <Junaid_sikander@live.com>
2021-01-02 17:04:44 +0500

    [ADD] order route

--- backend/server.js
+++ backend/server.js
@@ -3,6 +3,7 @@ import dotenv from 'dotenv'
 import connectDB from "./config/db.js";
 import productRouter from "./routes/productRoutes.js";
 import userRouter from "./routes/userRoutes.js";
+import orderRouter from "./routes/orderRoutes.js"
 
 import {errorHandler, notFound} from "./middlewares/errorMiddleware.js";
 
@@ -15,6 +16,7 @@ const PORT = process.env.PORT || 5000;
 
 app.use(express.json());
 app.use('/api/products', productRouter);
+app.use('/api/order', orderRouter);
 app.use('/api/users', userRouter);
 app.use(notFound);
 app.use(errorHandler);
