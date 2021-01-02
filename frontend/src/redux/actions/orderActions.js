f77f9f169f1579f4b196c07fd56fb53004f614c6
Junaid Sikander <Junaid_sikander@live.com>
2021-01-02 17:03:46 +0500

    [ADD] create order action, constants & reducer

---
+++ frontend/src/redux/actions/orderActions.js
@@ -0,0 +1,31 @@
+import {ORDER_CREATE_FAIL, ORDER_CREATE_REQUEST, ORDER_CREATE_SUCCESS} from "../constants/orderConstants";
+import axios from "axios";
+
+export const createOrder = (order) => async (dispatch, getState) => {
+    try {
+        dispatch({type: ORDER_CREATE_REQUEST});
+
+        const {userLogin: {userInfo}} = getState();
+
+        const config = {
+            headers: {
+                'Content-Type': 'application/json',
+                Authorization: `Bearer ${userInfo.token}`,
+            },
+        };
+
+        const {data} = await axios.post('/api/order', order, config);
+
+        dispatch({
+            type: ORDER_CREATE_SUCCESS,
+            payload: data
+        })
+
+    } catch (e) {
+        dispatch({
+            type: ORDER_CREATE_FAIL,
+            payload: e.response && e.response.data.message ?
+                e.response.data.message : e.message
+        })
+    }
+};
