15aa2ef597f399f06da5b293cd1b82f4e216e744
Junaid Sikander <Junaid_sikander@live.com>
2021-01-04 16:45:22 +0500

    [ADD] controller & route order by id enpoint

--- backend/controllers/orderController.js
+++ backend/controllers/orderController.js
@@ -28,4 +28,18 @@ const addOrderItems = asyncHandler(async (req, res) => {
 
 });
 
-export {addOrderItems};
+//@desc     Get Order by Id
+//@route    GET /api/order/:id
+//@access   Private
+const getOrderById = asyncHandler(async (req, res) => {
+    const order = await Order.findById(req.params.id).populate('user', 'name email');
+
+    if (order)
+        res.json(order);
+    else {
+        res.status(404);
+        throw new Error('Order not found')
+    }
+});
+
+export {addOrderItems, getOrderById};
--- backend/routes/orderRoutes.js
+++ backend/routes/orderRoutes.js
@@ -1,10 +1,11 @@
 import express from 'express'
 import {protect} from "../middlewares/authMiddleware.js";
-import {addOrderItems} from "../controllers/orderController.js";
+import {addOrderItems, getOrderById} from "../controllers/orderController.js";
 
 const orderRouter = express.Router();
 
 orderRouter.route('/').post(protect, addOrderItems);
+orderRouter.route('/:id').get(protect, getOrderById);
 
 export default orderRouter;
 
