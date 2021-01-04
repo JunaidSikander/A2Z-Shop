b60f7c917a1535503efd4aef3f9a56101184a366
Junaid Sikander <Junaid_sikander@live.com>
2021-01-04 20:44:43 +0500

    [ADD] get my orders controller & route

--- backend/controllers/orderController.js
+++ backend/controllers/orderController.js
@@ -67,4 +67,12 @@ const updateOrderToPaid = asyncHandler(async (req, res) => {
     }
 });
 
-export {addOrderItems, getOrderById, updateOrderToPaid};
+// @desc    Get logged in user orders
+// @route   GET /api/order/myorders
+// @access  Private
+const getMyOrders = asyncHandler(async (req, res) => {
+    const orders = await Order.find({user: req.user._id});
+    res.json(orders)
+});
+
+export {addOrderItems, getOrderById, updateOrderToPaid, getMyOrders};
--- backend/routes/orderRoutes.js
+++ backend/routes/orderRoutes.js
@@ -1,10 +1,11 @@
 import express from 'express'
 import {protect} from "../middlewares/authMiddleware.js";
-import {addOrderItems, getOrderById, updateOrderToPaid} from "../controllers/orderController.js";
+import {addOrderItems, getMyOrders, getOrderById, updateOrderToPaid} from "../controllers/orderController.js";
 
 const orderRouter = express.Router();
 
 orderRouter.route('/').post(protect, addOrderItems);
+orderRouter.route('/myorders').get(protect, getMyOrders);
 orderRouter.route('/:id').get(protect, getOrderById);
 orderRouter.route('/:id/pay').put(protect, updateOrderToPaid);
 
