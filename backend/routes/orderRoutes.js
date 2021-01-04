84861fc240bc8b7cfe2f64772d527e45cbc0f3df
Junaid Sikander <Junaid_sikander@live.com>
2021-01-04 18:38:50 +0500

    [ADD] update order to paid controler & route

--- backend/routes/orderRoutes.js
+++ backend/routes/orderRoutes.js
@@ -1,11 +1,12 @@
 import express from 'express'
 import {protect} from "../middlewares/authMiddleware.js";
-import {addOrderItems, getOrderById} from "../controllers/orderController.js";
+import {addOrderItems, getOrderById, updateOrderToPaid} from "../controllers/orderController.js";
 
 const orderRouter = express.Router();
 
 orderRouter.route('/').post(protect, addOrderItems);
 orderRouter.route('/:id').get(protect, getOrderById);
+orderRouter.route('/:id/pay').put(protect, updateOrderToPaid);
 
 export default orderRouter;
 
