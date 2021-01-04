caafc2a77838c84f329c602330d8e084fceb5b86
Junaid Sikander <Junaid_sikander@live.com>
2021-01-04 21:45:28 +0500

    [ADD] order my List action, constants & reducer

--- frontend/src/redux/actions/orderActions.js
+++ frontend/src/redux/actions/orderActions.js
@@ -5,6 +5,9 @@ import {
     ORDER_DETAILS_FAIL,
     ORDER_DETAILS_REQUEST,
     ORDER_DETAILS_SUCCESS,
+    ORDER_MY_LIST_FAIL,
+    ORDER_MY_LIST_REQUEST,
+    ORDER_MY_LIST_SUCCESS,
     ORDER_PAY_FAIL,
     ORDER_PAY_REQUEST,
     ORDER_PAY_SUCCESS
@@ -91,6 +94,34 @@ export const payOrder = (orderId, paymentResult) => async (dispatch, getState) =
     } catch (e) {
         dispatch({
             type: ORDER_PAY_FAIL,
+            payload: e.response && e.response.data.message ?
+                e.response.data.message : e.message
+        })
+    }
+};
+
+export const myOrderList = () => async (dispatch, getState) => {
+    try {
+        dispatch({type: ORDER_MY_LIST_REQUEST});
+
+        const {userLogin: {userInfo}} = getState();
+
+        const config = {
+            headers: {
+                Authorization: `Bearer ${userInfo.token}`
+            },
+        };
+
+        const {data} = await axios.get(`/api/order/myorders`, config);
+
+        dispatch({
+            type: ORDER_MY_LIST_SUCCESS,
+            payload: data
+        })
+
+    } catch (e) {
+        dispatch({
+            type: ORDER_MY_LIST_FAIL,
             payload: e.response && e.response.data.message ?
                 e.response.data.message : e.message
         })
