6d23db69ab27d90a2640650c4eeebd125fc8094b
Junaid Sikander <Junaid_sikander@live.com>
2021-01-05 22:07:41 +0500

    [ADD] user updates actions, constants & reducer

--- frontend/src/redux/actions/userActions.js
+++ frontend/src/redux/actions/userActions.js
@@ -18,9 +18,12 @@ import {
     USER_REGISTER_FAIL,
     USER_REGISTER_REQUEST,
     USER_REGISTER_SUCCESS,
+    USER_UPDATE_FAIL,
     USER_UPDATE_PROFILE_FAIL,
     USER_UPDATE_PROFILE_REQUEST,
-    USER_UPDATE_PROFILE_SUCCESS
+    USER_UPDATE_PROFILE_SUCCESS,
+    USER_UPDATE_REQUEST,
+    USER_UPDATE_SUCCESS
 } from "../constants/userConstants";
 import {ORDER_MY_LIST_RESET} from "../constants/orderConstants";
 
@@ -198,6 +201,33 @@ export const deleteUser = (id) => async (dispatch, getState) => {
     } catch (e) {
         dispatch({
             type: USER_DELETE_FAIL,
+            payload: e.response && e.response.data.message ?
+                e.response.data.message : e.message
+        })
+    }
+};
+
+export const updateUser = (user) => async (dispatch, getState) => {
+    try {
+        dispatch({type: USER_UPDATE_REQUEST});
+
+        const {userLogin: {userInfo}} = getState();
+
+        const config = {
+            headers: {
+                'Content-Type': 'application/json',
+                Authorization: `Bearer ${userInfo.token}`
+            }
+        };
+
+        const {data} = await axios.put(`/api/users/${user._id}`, user, config);
+
+        dispatch({type: USER_UPDATE_SUCCESS});
+        dispatch({type: USER_DETAILS_SUCCESS, payload: data});
+
+    } catch (e) {
+        dispatch({
+            type: USER_UPDATE_FAIL,
             payload: e.response && e.response.data.message ?
                 e.response.data.message : e.message
         })
