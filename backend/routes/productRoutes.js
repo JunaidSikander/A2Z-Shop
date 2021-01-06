cfd52a1fa40258b7da78441ddb39a7ee0dcd51da
Junaid Sikander <Junaid_sikander@live.com>
2021-01-06 13:40:45 +0500

    [ADD] create product POST request & update product PUT request

--- backend/routes/productRoutes.js
+++ backend/routes/productRoutes.js
@@ -1,15 +1,25 @@
 import express from 'express'
-import {deleteProduct, getProductById, getProducts} from "../controllers/productController.js";
+import {
+    createproduct,
+    deleteProduct,
+    getProductById,
+    getProducts,
+    updateProduct
+} from "../controllers/productController.js";
 import {admin, protect} from "../middlewares/authMiddleware.js";
 
 
 const productRouter = express.Router();
 
 
-productRouter.route('/').get(getProducts);
+productRouter
+    .route('/')
+    .get(getProducts)
+    .post(protect, admin, createproduct);
 productRouter
     .route('/:id')
     .get(getProductById)
+    .put(protect, admin, updateProduct)
     .delete(protect, admin, deleteProduct);
 
 export default productRouter;
