77e31c630a616f9acf10b275e602059ff2ce7187
Junaid Sikander <Junaid_sikander@live.com>
2021-01-07 11:50:10 +0500

    [ADD] get orders controller & route

--- backend/controllers/orderController.js
+++ backend/controllers/orderController.js
@@ -75,4 +75,17 @@ const getMyOrders = asyncHandler(async (req, res) => {
     res.json(orders)
 });
 
-export {addOrderItems, getOrderById, updateOrderToPaid, getMyOrders};
+// @desc    Get all orders
+// @route   GET /api/orders
+// @access  Private/Admin
+const getOrders = asyncHandler(async (req, res) => {
+    const orders = await Order.find({}).populate('user', 'id name');
+    if (orders)
+        res.json(orders);
+    else {
+        res.status(404);
+        throw new Error('Orders not found');
+    }
+});
+
+export {addOrderItems, getOrderById, updateOrderToPaid, getMyOrders, getOrders};
--- backend/routes/orderRoutes.js
+++ backend/routes/orderRoutes.js
@@ -1,10 +1,19 @@
 import express from 'express'
-import {protect} from "../middlewares/authMiddleware.js";
-import {addOrderItems, getMyOrders, getOrderById, updateOrderToPaid} from "../controllers/orderController.js";
+import {admin, protect} from "../middlewares/authMiddleware.js";
+import {
+    addOrderItems,
+    getMyOrders,
+    getOrderById,
+    getOrders,
+    updateOrderToPaid
+} from "../controllers/orderController.js";
 
 const orderRouter = express.Router();
 
-orderRouter.route('/').post(protect, addOrderItems);
+orderRouter
+    .route('/')
+    .post(protect, addOrderItems)
+    .get(protect, admin, getOrders);
 orderRouter.route('/myorders').get(protect, getMyOrders);
 orderRouter.route('/:id').get(protect, getOrderById);
 orderRouter.route('/:id/pay').put(protect, updateOrderToPaid);
