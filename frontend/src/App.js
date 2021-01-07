7691cf03953731b610e91d4423ec2fc8f99e7e20
Junaid Sikander <Junaid_sikander@live.com>
2021-01-07 21:58:28 +0500

    [ADD] Search Box Feature

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -26,6 +26,7 @@ const App = () => {
             <main className='py-3'>
                 <Container>
                     <Route exact path='/' component={HomeScreen}/>
+                    <Route path='/search/:keyword' component={HomeScreen}/>
                     <Route path='/product/:id' component={ProductScreen}/>
                     <Route path='/cart/:id?' component={CartScreen}/>
                     <Route path='/login' component={LoginScreen}/>
--- frontend/src/components/Header.js
+++ frontend/src/components/Header.js
@@ -1,8 +1,10 @@
 import React from 'react'
+import {Route} from 'react-router-dom'
 import {LinkContainer} from 'react-router-bootstrap'
 import {useDispatch, useSelector} from "react-redux";
 import {Container, Nav, Navbar, NavDropdown} from "react-bootstrap";
 import {logout} from "../redux/actions/userActions";
+import SearchBox from "./SearchBox";
 
 
 const Header = () => {
@@ -23,6 +25,7 @@ const Header = () => {
                 </LinkContainer>
                 <Navbar.Toggle aria-controls="basic-navbar-nav"/>
                 <Navbar.Collapse id="basic-navbar-nav">
+                    <Route render={({history}) => <SearchBox history={history}/>}/>
                     <Nav className="ml-auto">
                         <LinkContainer to='/cart'>
                             <Nav.Link>
