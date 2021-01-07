0543f86342814761762ef733f39a0e51eee878a5
Junaid Sikander <Junaid_sikander@live.com>
2021-01-07 11:50:59 +0500

    [ADD] order list action, constants & reducer

--- frontend/src/redux/actions/orderActions.js
+++ frontend/src/redux/actions/orderActions.js
@@ -5,6 +5,9 @@ import {
     ORDER_DETAILS_FAIL,
     ORDER_DETAILS_REQUEST,
     ORDER_DETAILS_SUCCESS,
+    ORDER_LIST_FAIL,
+    ORDER_LIST_REQUEST,
+    ORDER_LIST_SUCCESS,
     ORDER_MY_LIST_FAIL,
     ORDER_MY_LIST_REQUEST,
     ORDER_MY_LIST_SUCCESS,
@@ -122,6 +125,34 @@ export const myOrderList = () => async (dispatch, getState) => {
     } catch (e) {
         dispatch({
             type: ORDER_MY_LIST_FAIL,
+            payload: e.response && e.response.data.message ?
+                e.response.data.message : e.message
+        })
+    }
+};
+
+export const listOrders = () => async (dispatch, getState) => {
+    try {
+        dispatch({type: ORDER_LIST_REQUEST});
+
+        const {userLogin: {userInfo}} = getState();
+
+        const config = {
+            headers: {
+                Authorization: `Bearer ${userInfo.token}`
+            },
+        };
+
+        const {data} = await axios.get(`/api/order`, config);
+
+        dispatch({
+            type: ORDER_LIST_SUCCESS,
+            payload: data
+        })
+
+    } catch (e) {
+        dispatch({
+            type: ORDER_LIST_FAIL,
             payload: e.response && e.response.data.message ?
                 e.response.data.message : e.message
         })
--- frontend/src/redux/reducers/orderReducers.js
+++ frontend/src/redux/reducers/orderReducers.js
@@ -5,8 +5,13 @@ import {
     ORDER_DETAILS_FAIL,
     ORDER_DETAILS_REQUEST,
     ORDER_DETAILS_SUCCESS,
+    ORDER_LIST_FAIL,
+    ORDER_LIST_REQUEST,
+    ORDER_LIST_RESET,
+    ORDER_LIST_SUCCESS,
     ORDER_MY_LIST_FAIL,
-    ORDER_MY_LIST_REQUEST, ORDER_MY_LIST_RESET,
+    ORDER_MY_LIST_REQUEST,
+    ORDER_MY_LIST_RESET,
     ORDER_MY_LIST_SUCCESS,
     ORDER_PAY_FAIL,
     ORDER_PAY_REQUEST,
@@ -102,7 +107,30 @@ export const orderMyListReducer = (state = {orders: []}, action) => {
                 error: action.payload
             };
         case ORDER_MY_LIST_RESET:
-            return { orders: [] };
+            return {orders: []};
+        default:
+            return state;
+    }
+};
+
+export const orderListReducer = (state = {orders: []}, action) => {
+    switch (action.type) {
+        case ORDER_LIST_REQUEST:
+            return {
+                loading: true
+            };
+        case ORDER_LIST_SUCCESS:
+            return {
+                loading: false,
+                orders: action.payload
+            };
+        case ORDER_LIST_FAIL:
+            return {
+                loading: false,
+                error: action.payload
+            };
+        case ORDER_LIST_RESET:
+            return {orders: []};
         default:
             return state;
     }
