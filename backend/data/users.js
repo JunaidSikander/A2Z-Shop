395c7f2dff9ec523a9719b5666e291edb4e53f46
Junaid Sikander <Junaid_sikander@live.com>
2021-01-08 19:52:08 +0500

    [FIX] set one user should be admin

--- backend/data/users.js
+++ backend/data/users.js
@@ -5,6 +5,7 @@ const users = [
         name: "Admin User",
         email: 'admin@example1.com',
         password: bcrypt.hashSync('12345', 10),
+        isAdmin: true
     },
     {
         name: "Junaid Ali",
