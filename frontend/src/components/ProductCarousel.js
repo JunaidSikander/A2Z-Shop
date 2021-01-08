6f2bace4b5f49521863a179b246c3ee4a15c7a18
Junaid Sikander <Junaid_sikander@live.com>
2021-01-08 19:17:14 +0500

    [FIX] carousel layour

--- frontend/src/components/ProductCarousel.js
+++ frontend/src/components/ProductCarousel.js
@@ -25,11 +25,11 @@ const ProductCarousel = () => {
             {products.map((product) => (
                 <Carousel.Item key={product._id}>
                     <Link to={`/product/${product._id}`}>
-                        <Image src={product.image} alt={product.name}/>
+                        <Image src={product.image} alt={product.name} fluid/>
                         <Carousel.Caption className='carousel-caption'>
-                            <h2>
+                            <h3>
                                 {product.name} (${product.price})
-                            </h2>
+                            </h3>
                         </Carousel.Caption>
                     </Link>
                 </Carousel.Item>
